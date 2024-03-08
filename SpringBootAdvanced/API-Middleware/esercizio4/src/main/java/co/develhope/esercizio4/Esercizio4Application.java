5c9760b19c01cd01e501244e88251bd79db891e6
SimoneLiotta <simoneliotta99@gmail.com>
2024-03-08 18:54:50 +0100

    esercizio 4 finito

---
+++ SpringBootAdvanced/API-Middleware/esercizio4/src/main/java/co/develhope/esercizio4/Esercizio4Application.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio4Application.class, args);
+	}
+
+}
---
+++ SpringBootAdvanced/API-Middleware/esercizio4/src/test/java/co/develhope/esercizio4/Esercizio4ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
