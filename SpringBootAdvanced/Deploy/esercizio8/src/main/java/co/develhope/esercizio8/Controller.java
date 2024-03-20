861de0279d93e3d88bd2bc89d10c14fe7e741efb
SimoneLiotta <simoneliotta99@gmail.com>
2024-03-20 11:21:57 +0100

    esercizio finito

--- SpringBootAdvanced/Deploy/esercizio8/src/main/java/co/develhope/esercizio8/Controller.java
+++ SpringBootAdvanced/Deploy/esercizio8/src/main/java/co/develhope/esercizio8/Controller.java
@@ -2,7 +2,6 @@ package co.develhope.esercizio8;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 @RestController
 public class Controller {
-    @Autowired
-//    private Environment environment;
+
     @Value("${application.welcomeMsg}")
     String welcomeMsg;
 
